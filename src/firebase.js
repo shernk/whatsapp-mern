b09cc3074ddae852df5df560bb670e373e27070e
Sherk <phanquocthinh.dng@gmail.com>
2022-03-01 08:47:14 +0700

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDL1pUwcoT7mVqSBv2qwBiEU-seqIg8Kr0",
+  authDomain: "whatsapp-mern-01032022.firebaseapp.com",
+  projectId: "whatsapp-mern-01032022",
+  storageBucket: "whatsapp-mern-01032022.appspot.com",
+  messagingSenderId: "792391490069",
+  appId: "1:792391490069:web:e1b9ce708e6a6db8754c1c",
 };
